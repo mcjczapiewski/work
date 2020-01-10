0cde93535c58a0ca767929454c631baf12790e5c
mcjczapiewski <59090975+mcjczapiewski@users.noreply.github.com>
2020-01-10 18:50:05 +0100

    first one

---
+++ delete_empty_folders.py
@@ -0,0 +1,9 @@
+import os
+
+sciezka = input('Podaj ścieżkę: ')
+usun = []
+
+for subdir, dirs, _ in os.walk(sciezka):
+    if not os.listdir(subdir):
+        usun.append(subdir)
+        print(subdir)
