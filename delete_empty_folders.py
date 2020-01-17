fc333caaaa365f7f3630e2f62d596d0a979a94fd
mcjczapiewski <59090975+mcjczapiewski@users.noreply.github.com>
2020-01-17 10:45:15 +0100

    minor tweaks and comments

--- delete_empty_folders.py
+++ delete_empty_folders.py
@@ -2,63 +2,80 @@ import os
 from natsort import natsort_keygen
 
 nkey = natsort_keygen()
+again = "y"
 
 path = input("\nEnter the path: ")
 print("\n")
-delete = []
-number = 0
 
-for subdir, dirs, _ in os.walk(path):
-    dirs.sort(key=nkey)
-    if "zrobione" not in subdir.lower() and not os.listdir(subdir):
-        delete.append(subdir)
-        print(str(number) + "\t" + subdir)
-        number += 1
+while again == "y":
+    delete = []
+    number = 0
 
-if delete:
-    is_it = input(
-        """
+    # iterates through folders and adding to list empty once
+    for subdir, dirs, _ in os.walk(path):
+        dirs.sort(key=nkey)
+        if "zrobione" not in subdir.lower() and not os.listdir(subdir):
+            delete.append(subdir)
+            print(str(number) + "\t" + subdir)
+            number += 1
+
+    # if there is something in list, ask to delete them
+    if delete:
+        is_it = input(
+            """
 Delete the listed folders? - y
 End the program without deleting? - n
 Remove some directories from list? - r
 y/n/r: """
-    )
+        )
 
-    while is_it.lower() == "r":
-        which = input(
-            """
+        # iterates through list to remove some entries
+        while is_it.lower() == "r":
+            which = input(
+                """
 Which line do you want to delete?
 Press any key but number from list if you want to abort.
 Go from the last to first number while deleting: """
-        )
-        try:
-            delete.pop(int(which))
-        except (ValueError, IndexError):
-            print(("\nNone directory was deleted. Action aborted.").upper())
-            pass
-        is_it = input(
-            """
+            )
+            try:
+                delete.pop(int(which))
+            except (ValueError, IndexError):
+                print(
+                    ("\nNone directory was deleted. Action aborted.").upper()
+                )
+                pass
+            is_it = input(
+                """
 Delete the remaining folders? - y
 End the program without deleting? - n
 Remove more directories from list? - r
 y/n/r: """
-        )
-        if is_it.lower() == "r":
-            number = 0
-            for item in delete:
-                print(str(number) + "\t" + item)
-                number += 1
+            )
+            # if user wants to remove more from list,
+            # its printing the list
+            if is_it.lower() == "r":
+                number = 0
+                for item in delete:
+                    print(str(number) + "\t" + item)
+                    number += 1
 
-    if is_it.lower() == "y":
-        ile = 1
-        for item in delete:
-            os.rmdir(item)
-            print(ile)
-            ile += 1
-        print("~~~THE END.~~~\n")
+        # removing directories if user said so
+        if is_it.lower() == "y":
+            ile = 1
+            for item in delete:
+                os.rmdir(item)
+                print(ile)
+                ile += 1
+            print("~~~THE END.~~~\n")
+        # ends if user don't want to delete
+        else:
+            print("~~~THE END.~~~\n")
 
     else:
-        print("~~~THE END.~~~\n")
+        print("~~~NO EMPTY FOLDERS.~~~\n")
+        break
 
-else:
-    print("~~~NO EMPTY FOLDERS.~~~\n")
+    # looping after deleting folders
+    again = input("Again?  y/n: ")
+
+print("~~~EXITED.~~~\n")
