841e01de0d2ba28a1e4602e113150ded6dee08b7
mcjczapiewski <59090975+mcjczapiewski@users.noreply.github.com>
2020-01-31 10:23:10 +0100

    writing lines from files as well as direcotries

--- czy_opis_ma_dzialki_prawne.py
+++ czy_opis_ma_dzialki_prawne.py
@@ -4,17 +4,20 @@ from natsort import natsort_keygen
 
 nkey = natsort_keygen()
 
-for subdir, dirs, _ in os.walk(r"J:\0418102"):
+for subdir, dirs, _ in os.walk(r"D:\_MACIEK_\python_proby\opisy_2.1"):
     dirs.sort(key=nkey)
     if "PRAWNE" in subdir and os.path.exists(os.path.join(subdir, "opis.txt")):
         opis = os.path.join(subdir, "opis.txt")
         with io.open(
-            r"J:\0418102\zebrane.txt", "a", encoding="utf-8"
+            r"D:\_MACIEK_\python_proby\opisy_2.1\zebrane.txt", "a", encoding="utf-8"  # noqa
         ) as zebrane:
             try:
                 with open(opis, "r") as ooo:
                     if not any("|" in line for line in ooo):
-                        zebrane.write(subdir + "\n")
+                        ooo.seek(0)
+                        zebrane.write("\n\n" + subdir + "\n")
+                        for line in ooo:
+                            zebrane.write(line)
             # except UnicodeDecodeError:
             #     with io.open(opis, "r", encoding="utf-8") as ooo:
             #         if not any("|" in line for line in ooo):
