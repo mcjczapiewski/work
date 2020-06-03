ee3d953ffad1b8caec20f6db5a6697397f94c6ba
mcjczapiewski <59090975+mcjczapiewski@users.noreply.github.com>
2020-06-03 12:43:23 +0200

    New paths

--- poprawnosc_nazw_plikow_na_dysku_p33.py
+++ poprawnosc_nazw_plikow_na_dysku_p33.py
@@ -20,60 +20,60 @@ nazwy = (
 )
 
 # path = input("Enter the path: ")
-# path = r"I:\INOWROCŁAW\DANE_IRON_MOUNTAIN\20190614\ZADANIE 3\ZŁOTNIKI KUJAWSKIE"  # noqa
-errors = r"I:\INOWROCŁAW\DANE_IRON_MOUNTAIN\20190614\ZADANIE 2\04_KOPIA_PLIKOWA\kontrola_2020-05-14"
+path = r"I:\INOWROCŁAW\DANE_IRON_MOUNTAIN\20190614\ZADANIE 4\M.JANIKOWO"  # noqa
+errors = r"I:\INOWROCŁAW\DANE_IRON_MOUNTAIN\20190614\ZADANIE 4\M.JANIKOWO\kontrole"
 
 count = 1
 
-with open(
-    r"I:\INOWROCŁAW\DANE_IRON_MOUNTAIN\20190614\ZADANIE 2\04_KOPIA_PLIKOWA\sciezki.txt",
-    "r",
-    encoding="utf-8",
-) as sciezki:
-    for line in sciezki:
-        path = line.strip()
-        for subdir, dirs, files in os.walk(path):
-            dirs.sort(key=nkey)
+# with open(
+#     r"I:\INOWROCŁAW\DANE_IRON_MOUNTAIN\20190614\ZADANIE 3\kontrole_2020-05-22\sciezki.txt",
+#     "r",
+#     encoding="utf-8",
+# ) as sciezki:
+#     for line in sciezki:
+#         path = line.strip()
+for subdir, dirs, files in os.walk(path):
+    dirs.sort(key=nkey)
 
-            if not any(
-                fname.upper().endswith(".PDF") for fname in os.listdir(subdir)
-            ) or not os.path.basename(subdir).startswith("P"):
-                continue
+    if not any(
+        fname.upper().endswith(".PDF") for fname in os.listdir(subdir)
+    ) or not os.path.basename(subdir).startswith("P"):
+        continue
 
-            print(
-                str(count)
-                + "\t"
-                + os.path.basename(os.path.dirname(subdir))
-                + "\\"
-                + os.path.basename(subdir)
-            )
-            count += 1
+    print(
+        str(count)
+        + "\t"
+        + os.path.basename(os.path.dirname(subdir))
+        + "\\"
+        + os.path.basename(subdir)
+    )
+    count += 1
 
-            for file in natsorted(files):
-                if file.upper().endswith(".PDF"):
-                    if not regex.match(r"^P.+[0-9]\.PDF", file.upper()):
-                        with io.open(
-                            fr"{errors}\niepoprawne_nazwy.txt",  # noqa
-                            "a",
-                            encoding="utf-8",
-                        ) as np:
-                            np.write(os.path.join(subdir, file) + "\n")
-                    else:
-                        try:
-                            nazwa = regex.match(
-                                r"^.+?-(.+[A-Z])-[0-9].+PDF", file.upper()
-                            )[1]
-                        except:
-                            with io.open(
-                                fr"{errors}\niepoprawne_nazwy.txt",  # noqa
-                                "a",
-                                encoding="utf-8",
-                            ) as np:
-                                np.write(os.path.join(subdir, file) + "\n")
-                        if nazwa not in nazwy:
-                            with io.open(
-                                fr"{errors}\niepoprawne_nazwy.txt",  # noqa
-                                "a",
-                                encoding="utf-8",
-                            ) as np:
-                                np.write(os.path.join(subdir, file) + "\n")
+    for file in natsorted(files):
+        if file.upper().endswith(".PDF"):
+            if not regex.match(r"^P.+[0-9]\.PDF", file.upper()):
+                with io.open(
+                    fr"{errors}\niepoprawne_nazwy.txt",  # noqa
+                    "a",
+                    encoding="utf-8",
+                ) as np:
+                    np.write(os.path.join(subdir, file) + "\n")
+            else:
+                try:
+                    nazwa = regex.match(
+                        r"^.+?-(.+[A-Z])-[0-9].+PDF", file.upper()
+                    )[1]
+                except:
+                    with io.open(
+                        fr"{errors}\niepoprawne_nazwy.txt",  # noqa
+                        "a",
+                        encoding="utf-8",
+                    ) as np:
+                        np.write(os.path.join(subdir, file) + "\n")
+                if nazwa not in nazwy:
+                    with io.open(
+                        fr"{errors}\niepoprawne_nazwy.txt",  # noqa
+                        "a",
+                        encoding="utf-8",
+                    ) as np:
+                        np.write(os.path.join(subdir, file) + "\n")
