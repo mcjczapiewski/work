372733db3f7d3b0906bbc8030e6e75c14b4afafa
mcjczapiewski <59090975+mcjczapiewski@users.noreply.github.com>
2020-06-05 07:08:01 +0200

    Initial of take_out_from_merge

---
+++ take_out_from_merge.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+count = 1
+
+for subdir, dirs, _ in os.walk(r'P:\cyfryzacja_powiat_inowroclawski\SKANY'):
+    if r'SKANY\040701_2' in subdir:
+        continue
+    merge = os.path.join(subdir, os.path.basename(subdir), "merge")
+    if os.path.exists(merge):
+        print(count)
+        count += 1
+        main_folder = subdir
+        for _, _, files in os.walk(merge):
+            for file in files:
+                if not file.lower().endswith(".pdf"):
+                    continue
+                from_here = os.path.join(merge, file)
+                shutil.move(from_here, main_folder)
+        sub_folder = os.path.join(main_folder, os.path.basename(main_folder))
+        if os.path.exists(sub_folder):
+            shutil.rmtree(sub_folder)
