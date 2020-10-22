d8ea769af4bec81fc6136ce6df1fc66c18f6bb46
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-10-22 12:54:08 +0200

    Add new files

--- check_if_all_named_inowroclaw.py
+++ check_if_all_named_inowroclaw.py
@@ -1,31 +1,50 @@
 import os
+import msvcrt
 from natsort import natsort_keygen
 
-path = r"P:\cyfryzacja_powiat_inowroclawski\SKANY\040701_1\prawne\040701_1.0001"
+path = r"D:\WPG\inowroclawski\040701_1.0006"
 # path = input("Podaj ścieżkę: ")
+again = "t"
 
-for subdir, dirs, _ in os.walk(path):
-    dirs.sort(key=natsort_keygen())
-    steps_inside_path = len(subdir.split("\\"))
-    if steps_inside_path == 7:
-        up_to_merge = os.path.join(subdir, os.path.basename(subdir), "merge")
-        is_there_basename = os.path.join(subdir, os.path.basename(subdir))
-        if os.path.exists(up_to_merge):
-            if not any(
-                fname.upper().endswith(".PDF")
-                for fname in os.listdir(up_to_merge)
-            ):
+while again == "t":
+    count = 1
+    pdfs_to_name = 0
+    for subdir, dirs, _ in os.walk(path):
+        dirs.sort(key=natsort_keygen())
+        steps_inside_path = len(subdir.split("\\"))
+        if steps_inside_path == 5:
+            up_to_merge = os.path.join(
+                subdir, os.path.basename(subdir), "merge"
+            )
+            is_there_basename = os.path.join(subdir, os.path.basename(subdir))
+            if os.path.exists(up_to_merge):
+                if not any(
+                    fname.upper().endswith(".PDF")
+                    for fname in os.listdir(up_to_merge)
+                ):
+                    if not any(
+                        fname.upper().endswith(".PDF")
+                        for fname in os.listdir(is_there_basename)
+                    ):
+                        pdfs_to_name += len(os.listdir(subdir))
+                        print(f"{subdir}")
+                        count += 1
+            elif os.path.exists(is_there_basename):
                 if not any(
                     fname.upper().endswith(".PDF")
                     for fname in os.listdir(is_there_basename)
                 ):
-                    print(subdir)
-        elif os.path.exists(is_there_basename):
-            print(os.listdir(is_there_basename))
-            if not any(
-                fname.upper().endswith(".PDF")
-                for fname in os.listdir(is_there_basename)
-            ):
-                print(subdir)
-        else:
-            print(subdir)
+                    pdfs_to_name += len(os.listdir(subdir))
+                    print(f"{subdir}")
+                    count += 1
+            else:
+                pdfs_to_name += len(os.listdir(subdir))
+                print(f"{subdir}")
+                count += 1
+
+    print(f"\nPozostało {count} operatów, {pdfs_to_name} pdfów do nazwania.")
+    print("Powtórzyć? t/n\n> ", end="")
+    again = msvcrt.getwche().lower()
+    print()
+
+input("KONIEC.")
--- count_if_file_named.py
+++ count_if_file_named.py
@@ -1,22 +1,31 @@
 import os
 import datetime
+import msvcrt
 
 today = datetime.datetime.now().date()
 
-path = r"P:\cyfryzacja_powiat_inowroclawski\SKANY\040701_1\prawne\040701_1.0001"
+path = r"D:\WPG\inowroclawski\040701_1.0006"
 # path = input("Wklej ścieżkę do folderu głównego: ")
-count_all = count_today = 0
+again = "t"
 
-for subdir, dirs, files in os.walk(path):
-    if os.path.basename(subdir) in os.listdir(subdir):
-        folder_creation_time = datetime.date.fromtimestamp(
-            os.path.getctime(os.path.join(subdir, os.path.basename(subdir)))
-        )
-        for file in files:
-            count_all += 1
-            if folder_creation_time == today:
-                count_today += 1
+while again == "t":
+    count_all = count_today = 0
+    current_time = datetime.datetime.now().time()
+    print(f"\n{current_time.hour}:{str(current_time.minute).zfill(2)}")
+    for subdir, dirs, files in os.walk(path):
+        if os.path.basename(subdir) in os.listdir(subdir):
+            folder_creation_time = datetime.date.fromtimestamp(
+                os.path.getctime(os.path.join(subdir, os.path.basename(subdir)))
+            )
+            for file in files:
+                count_all += 1
+                if folder_creation_time == today:
+                    count_today += 1
 
-print("ZROBIONE OGÓŁEM: " + str(count_all))
-print("ZROBIONE DZISIAJ: " + str(count_today))
+    print(f"ZROBIONE OGÓŁEM: {count_all}")
+    print(f"ZROBIONE DZISIAJ: {count_today}")
+    print("Powtórzyć? t/n\n> ", end="")
+    again = msvcrt.getwche().lower()
+    print()
+
 input("KONIEC.")
